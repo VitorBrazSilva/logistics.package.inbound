e9fb2c11b456e0220a18f7fda2c3272a910e6a99
VitorBrazSilva <vitorbraz_@hotmail.com>
2025-03-06 15:13:16 -0300

    criação entidade Pacote

---
+++ src/main/java/mercadolivre/processoseletivo/Inbound/entity/Pacote.java
@@ -0,0 +1,51 @@
+package mercadolivre.processoseletivo.Inbound.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import mercadolivre.processoseletivo.Inbound.enums.PacoteStatus;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "pacotes")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class Pacote {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String sender;
+
+    @Column(nullable = false)
+    private String recipient;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PacoteStatus status;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deliveredAt;
+
+    private boolean isHoliday;
+
+    private String funFact;
+}
---
+++ src/main/java/mercadolivre/processoseletivo/Inbound/enums/PacoteStatus.java
@@ -0,0 +1,5 @@
+package mercadolivre.processoseletivo.Inbound.enums;
+
+public enum PacoteStatus {
+    CREATED, IN_TRANSIT, DELIVERED, CANCELED
+}
